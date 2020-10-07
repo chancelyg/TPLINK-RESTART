b186401122536927889716fe5374ab8fcd6ae035
chancel <ycs1026@vip.qq.com>
2020-10-07 14:24:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 1. TP-Link 路由器重启脚本
+测试产品型号：TL-WDR7650千兆易展版
+
+因为该产品型号无自带定时重启，利用家庭服务器定时执行脚本以重启路由器
+
+# 2. 使用方法
+安装如下环境
+- Python3
+
+需依赖以下包
+```Shell
+pip3 install PyYAML
+pip3 install requests
+```
+
+直接运行
+```
+python3 app.py
+```
---
+++ app.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import os
+import yaml
+import logging
+import logging.config
+
+_ROUTE_URL = '192.168.11.1'
+_PASSS_WD = 'QpQLBVBKg44fbwK'
+
+# 日志配置
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+with open('logging.yaml', 'r', encoding='utf-8') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+    logging.config.dictConfig(config)
+logger = logging.getLogger('main.common')
+
+def restart():
+    ''' Restart TP-Link Route
+    '''
+    url = 'http://%s/' % _ROUTE_URL
+    headers = {'Accept':'application/json, text/javascript, */*; q=0.01'}
+    login_dict = {
+        'method':'do',
+        'login':{
+            'password':_PASSS_WD
+        }
+    }
+    stok = None
+    logger.info('Login route url->%s，Login args->%s' % (url,json.dumps(login_dict)))
+    response = requests.post(url=url,headers=headers,json=login_dict)
+    if response.ok:
+        logger.info('Login return response http code is 200, Response text->%s' % response.text)
+        response_result = json.loads(response.text)
+        stok = response_result['stok']
+
+    if not stok:
+        logger.warn('Login is success. But not get stok!!')
+        return
+
+    url = 'http://%s/stok=%s/ds' % (_ROUTE_URL,stok)
+    restart_dict = {
+        'method':'do',
+        'hyfi':{
+            'reboot_all':None
+        }
+    }
+    logger.info('Post restart route. Post info->%s. Post args->%s' % (url,json.dumps(restart_dict)))
+    response = requests.post(url=url,headers=headers,json=restart_dict)
+    if response.ok:
+        logger.info('Restart is success.')
+
+if __name__ == '__main__':
+    logger.info('Start running. Route url is %s' % _ROUTE_URL)
+    restart()
+    logger.info('Running end')
+
